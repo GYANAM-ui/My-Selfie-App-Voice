330dd2b3d81e1a4b7c0dcf26722b1c080524024e
GYANAM-DEVELOPER <74140267+GYANAM-ui@users.noreply.github.com>
2021-02-28 11:51:30 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -12,27 +12,49 @@ recognition.onresult = function run (event) {
     var Content = event.results[0][0].transcript;
     console.log(Content);
     document.getElementById("textbox").innerHTML = Content;
-    speak();
+    
+    if(Content == "take my selfie")
+    {
+        console.log("taking selfie ---");
+        speak();
+    };
 }
 
 function speak()
 {
     var synth = window.speechSynthesis;
 
-    speak_data = document.getElementById("textbox").value;
+    speak_data = "Taking Your Selfie In 5 Seconds";
 
     var utterThis = new SpeechSynthesisUtterance(speak_data);
 
     synth.speak(utterThis);
     Webcam.attach(camera);
-
+    setTimeout(function(){
+        take_snapshot();
+        save();
+    },5000);
 }
 
 Webcam.set({
     width: 390,
     height: 280,
     image_format: 'jpeg',
-    png_quality: jpeg
+    jpeg_quality: 90
 });
 camera = document.getElementById("camera");
 
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML = '<img id="selfie_image" src="'+data_uri+'"/>';
+    });
+}
+
+function save()
+{
+    link = document.getElementById("link");
+    image = document.getElementById("selfie_image").src;
+    link.href = image;
+    link.click();
+}
